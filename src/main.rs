be7e2a38fe8d57a3c0b43a2d3eee77e24e2e442b
masato <04masato05@gmail.com>
2024-10-26 19:55:40 +0900

    カレントディレクトリのファイル名を表示できるように

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // カレントディレクトリを取得
+    let current_dir = env::current_dir().expect("Could not get current directory");
+
+    list_files(&current_dir);
+}
+
+fn list_files(dir: &std::path::Path) {
+    let entries = fs::read_dir(dir).expect("Could not read directory");
+
+    for entry in entries {
+        let entry = entry.expect("Error reading etnry");
+
+        if let Some(file_name) = entry.file_name().to_str() {
+            println!("{}", file_name);
+        }
+    }
 }
